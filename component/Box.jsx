8cea08b01ae765d0b681285134e1f8bde585d73f
jenodinerifat59 <mdjenodineislamrifat@gmail.com>
2026-02-12 20:17:55 +0600

    Add Pillars component

--- component/Box.jsx
+++ component/Box.jsx
@@ -3,7 +3,7 @@ import Image from './Image'
 
 const Box = ({src, heading, para}) => {
   return (
-    <div className=' rounded-lg p-6 w-fit bg-white'>
+    <div className=' rounded-lg p-6 w-fit bg-white shadow-md'>
         <Image className='' src={src} alt='icon' />
         <h4 className='font-bold text-xl mt-6 mb-3 '>{heading}</h4>
         <p className='font-normal text-[16px] w-63.5  '>{para} </p>
--- ui/Pillars.jsx
+++ ui/Pillars.jsx
@@ -6,7 +6,7 @@ const Pillars = () => {
   return (
     <div>
       <Heading headding='Pillars of "Bangladesh 2.0"'/>
-      <div className='grid grid-cols-4 gap-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
         <Box src={Icon} heading='Innovation' para='Dolor sit amet consectetur elit. Facilis quas, ducimus labore sequi quos '/>
         <Box src={Icon} heading='Innovation' para='Dolor sit amet consectetur elit. Facilis quas, ducimus labore sequi quos '/>
         <Box src={Icon}  heading='Innovation' para='Dolor sit amet consectetur elit. Facilis quas, ducimus labore sequi quos '/>
